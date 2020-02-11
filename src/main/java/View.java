2d8eb90badb0c24ec0d4b073be127232097097ba
oksana <oksanaov@gmail.com>
2020-02-11 16:31:22 +0200

    wrong input string added

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,5 +1,6 @@
 public class View {
     public static final String INPUT_PROMPT= "Input your name:";
+    public static final String WRONG_INPUT = "Wong input! Try again.";
 
     public void printMessage(String message){
         System.out.println(message);
