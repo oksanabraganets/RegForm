d6040bd9873fc39c360eb20ff2dea7a969b0eb88
oksana <oksanaov@gmail.com>
2020-02-11 16:30:47 +0200

    name reading added

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Controller {
+    private static final String NAME_REGEX = "[A-Z][a-z]{1,20}";
     private View view;
 
-    public Controller(View view){
+    public Controller(View view) {
         this.view = view;
     }
-    public void processUser(){
+
+    public void processUser() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(inputStringWithRegEx(sc));
+    }
+
+    public String inputStringWithRegEx(Scanner sc) {
         view.printMessage(view.INPUT_PROMPT);
+
+        while (! sc.hasNextLine() && sc.nextLine().matches(NAME_REGEX)){
+            view.printMessage(view.WRONG_INPUT);
+            sc.next();
+        }
+        System.out.println("good name!");
+        return sc.nextLine();
     }
 }
