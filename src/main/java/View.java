93980610f851d7b2749e06f375b12160c126704c
oksana <oksanaov@gmail.com>
2020-02-12 11:13:10 +0200

    strings are initialized from resource bundle

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,7 +1,20 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class View {
-    public static final String NAME_PROMPT= "Input your name:";
-    public static final String LOGIN_PROMPT= "Input your login:";
-    public static final String WRONG_INPUT = "Wong input! Try again.";
+    public static String NAME_PROMPT;
+    public static String LOGIN_PROMPT;
+    public static String WRONG_INPUT;
+
+    public View(){
+        String sourceName = "text";
+        String lang = "ua";
+        Locale locale = new Locale(lang);
+        ResourceBundle rb = ResourceBundle.getBundle("text",locale);
+        NAME_PROMPT = rb.getString("name");
+        LOGIN_PROMPT = rb.getString("login");
+        WRONG_INPUT = rb.getString("wrong");
+    }
 
     public void printMessage(String message){
         System.out.println(message);
